55511cac4bd35bb28218f302d5634ef9dbca40c3
Logan Cole <emtlogancole@gmail.com>
2023-12-18 16:57:29 -0600

    test: resident index

---
+++ spec/features/residents/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Resident Index Page' do
+  before :each do
+    @r1 = Resident.new(name: 'Logan', age: '69', occupation: 'street sweeper')
+    @r2 = Resident.new(name: 'Carol', age: '1000', occupation: 'who cares')
+  end
+
+  it 'has resident information' do
+    visit resident_path
+
+    expect(page).to have_content('Logan')
+    expect(page).to have_content('Carol')
+    expect(page).to have_content('69')
+    expect(page).to have_content('1000')
+    expect(page).to have_content('street sweeper')
+    expect(page).to have_content('who cares')
+  end
+end
