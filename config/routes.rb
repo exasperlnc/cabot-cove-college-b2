41c8eb7a30b6c8a9ecc082af3b5766e6f10f1300
Logan Cole <emtlogancole@gmail.com>
2023-12-27 16:01:58 -0600

    course index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/residents', to: 'resident#index', as: 'resident_path'
   get '/residents/:id', to: 'resident#show'
+  get '/courses', to: 'course#index'
 end
